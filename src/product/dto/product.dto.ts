fc7ffb2151566fcd260743df9b7b64c9a2db7687
“a84379681” <anas.mohammad1@h-partners.com>
2025-01-27 13:44:46 +0200

    add product swagger docs

--- src/product/dto/product.dto.ts
+++ src/product/dto/product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsInt,
   IsNotEmpty,
@@ -7,29 +8,37 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({ description: 'Name of the product', example: 'Product Name' })
   @IsString()
   @IsNotEmpty()
   product_name: string;
+  @ApiProperty({ description: 'Description of the product', example: 'Product Description' })
   @IsString()
   @IsNotEmpty()
   product_description: string;
+  @ApiProperty({ description: 'Price of the product', example: 99.99 })
   @IsNumber()
   @IsNotEmpty()
   price: number;
+  @ApiProperty({ description: 'stock_quantity of the product', example: 56 })
   @IsInt()
   @IsNotEmpty()
   stock_quantity: number;
 }
 export class UpdateProductDto {
+  @ApiProperty({ description: 'Name of the product', example: 'Product Name' })
   @IsString()
   @IsOptional()
   product_name: string;
+  @ApiProperty({ description: 'Description of the product', example: 'Product Description' })
   @IsString()
   @IsOptional()
   product_description: string;
+  @ApiProperty({ description: 'Price of the product', example: 99.99 })
   @IsNumber()
   @IsOptional()
   price_: number;
+  @ApiProperty({ description: 'stock_quantity of the product', example: 56 })
   @IsInt()
   @IsOptional()
   stock_quantity: number;
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -13,18 +13,30 @@ import { ProductService } from './product.service';
 import { CreateProductDto, UpdateProductDto } from './dto/product.dto';
 import { JwtAuthGuard } from 'src/jwtAuthGuard';
 import { RoleGuard } from 'src/roleAuthGuard';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 @UseGuards(JwtAuthGuard)
 @Controller('product')
 export class ProductController {
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService) { }
 
   //get all products
   @Get()
+  @ApiOperation({ summary: 'Get all products' })
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'Products retrieved successfully' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   getAllProducts() {
     return this.productService.getAllProducts();
   }
   //get products list based on queries
   @Get('/list')
+  @ApiOperation({ summary: 'Get product list' })
+  @ApiBearerAuth()
+  @ApiQuery({ name: 'limit', description: 'Limit number of products', required: false })
+  @ApiQuery({ name: 'page', description: 'Page number', required: false })
+  @ApiQuery({ name: 'product_type', description: 'Type of product', required: false })
+  @ApiResponse({ status: 200, description: 'Product list retrieved successfully' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   getProductList(
     @Query() query: { limit: string; page: string; product_type: string },
   ) {
@@ -32,12 +44,23 @@ export class ProductController {
   }
   //get product by id
   @Get('/:product_id')
+  @ApiOperation({ summary: 'Get product by ID' })
+  @ApiBearerAuth()
+  @ApiParam({ name: 'product_id', description: 'ID of the product' })
+  @ApiResponse({ status: 200, description: 'Product retrieved successfully' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   getProductById(@Param('product_id') product_id: string) {
     return this.productService.getProductById(+product_id);
   }
   //add product
   @UseGuards(RoleGuard)
   @Post()
+  @ApiOperation({ summary: 'Add a new product' })
+  @ApiBearerAuth()
+  @ApiBody({ type: CreateProductDto })
+  @ApiResponse({ status: 201, description: 'Product added successfully' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiBadRequestResponse({ description: 'Invalid input data' })
   addProduct(@Body() dto: CreateProductDto) {
     return this.productService.addProduct(dto);
   }
@@ -49,6 +72,13 @@ export class ProductController {
   //update product by id
   @UseGuards(RoleGuard)
   @Patch('/:product_id')
+  @ApiOperation({ summary: 'Update product information' })
+  @ApiBearerAuth()
+  @ApiBody({ type: UpdateProductDto })
+  @ApiParam({ name: 'product_id', description: 'ID of the product' })
+  @ApiResponse({ status: 200, description: 'Product updated successfully' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiBadRequestResponse({ description: 'Product ID is missing or must be an integer' })
   updateProduct(
     @Body() dto: UpdateProductDto,
     @Param('product_id') product_id: string,
@@ -58,11 +88,20 @@ export class ProductController {
   //delete product by id
   @UseGuards(RoleGuard)
   @Delete('/:product_id')
+  @ApiOperation({ summary: 'Delete product by ID' })
+  @ApiBearerAuth()
+  @ApiParam({ name: 'product_id', description: 'ID of the product' })
+  @ApiResponse({ status: 200, description: 'Product deleted successfully' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiBadRequestResponse({ description: 'Product ID is missing or must be an integer' })
   deleteProductById(@Param('product_id') product_id: string) {
     return this.productService.deleteProductById(+product_id);
   }
   //delete all products
   @UseGuards(RoleGuard)
+  @ApiOperation({ summary: 'Delete all products' })
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'All products deleted successfully' })
   @Delete()
   deleteAllProducts() {
     return this.productService.deleteAllProducts();
